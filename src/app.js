00fffe5f96a8cfc903f83a4f9aa1b114b262b796
KimDongHwan <kdhn712@gmail.com>
2024-12-04 20:14:56 +0900

    ✨ update 선수관련 라우터 임포트 추가

--- src/app.js
+++ src/app.js
@@ -1,13 +1,14 @@
 import express from 'express';
 import cookieParser from 'cookie-parser';
 import AccountsRouter from './routes/accounts.router.js';
+import PlayersRouter from './routes/players.router.js';
 
 const app = express();
 const PORT = 3018;
 
 app.use(express.json());
 app.use(cookieParser());
-app.use('/api', [AccountsRouter]);
+app.use('/api', [AccountsRouter, PlayersRouter]);
 
 app.listen(PORT, () => {
   console.log(PORT, '포트로 서버가 열렸어요!');
