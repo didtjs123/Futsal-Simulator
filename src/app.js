c0a7448a417367ac0a25cc9faa733aec5ea8f4a2
KimDongHwan <kdhn712@gmail.com>
2024-12-04 16:07:43 +0900

    commit

---
+++ src/app.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import cookieParser from 'cookie-parser';
+import AccountsRouter from './routes/accounts.router.js';
+
+const app = express();
+const PORT = 3018;
+
+app.use(express.json());
+app.use(cookieParser());
+app.use('/api', [AccountsRouter]);
+
+app.listen(PORT, () => {
+  console.log(PORT, '포트로 서버가 열렸어요!');
+});
